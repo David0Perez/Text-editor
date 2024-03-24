a9f3428a8fbe2e2e7f1e837965785830e5b6ef18
David0Perez <147769786+David0Perez@users.noreply.github.com>
2024-03-24 14:29:42 -0400

    db/install/webpack.js

--- 02-Challenge/Develop/client/webpack.config.js
+++ 02-Challenge/Develop/client/webpack.config.js
@@ -18,12 +18,42 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        inject: true,
+      }),
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'A simple text editor',
+        background_color: '#01579b',
+        theme_color: '#01579b',
+        start_url: '/',
+        icons: [
+          {
+            src: path.resolve('src/assets/icons/icon-512x512.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
